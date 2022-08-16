ce1f8d0a2b9c9dd1a0a66fa3b95c243bc1a563bc
sonia <sofiashchukina@outlook.com>
2022-08-16 12:43:16 +0200

    change build.yml

--- backend/src/main/java/sonia/meetapp/app/participants/ParticipantsController.java
+++ backend/src/main/java/sonia/meetapp/app/participants/ParticipantsController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class ParticipantsController{
     @GetMapping("/hello")
     String sayHello(){
-        return "Hallo";
+        return "Hello";
     }
 }
