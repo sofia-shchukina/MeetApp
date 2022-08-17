1f159a7e42c48e737ffaba2e0be558bc17df7e9a
sonia <sofiashchukina@outlook.com>
2022-08-17 21:27:33 +0200

    add service test

--- backend/src/main/java/sonia/meetapp/app/participants/Participant.java
+++ backend/src/main/java/sonia/meetapp/app/participants/Participant.java
@@ -6,4 +6,11 @@ import lombok.Data;
 public class Participant {
     String name;
     String id;
+
+    public Participant(String name, String id) {
+        this.name = name;
+        this.id = id;
+    }
+
+
 }
