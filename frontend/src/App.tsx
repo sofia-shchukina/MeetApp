0af7e4e31984734cfda241aa3b1841de1efc27a1
sonia <sofiashchukina@outlook.com>
2022-08-17 21:57:54 +0200

    delete classes that are not for first deployment

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,8 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 
 import './App.css';
 import axios from "axios";
-import {Participant} from "./Participant";
+
 
 export default function App() {
 
@@ -12,21 +12,8 @@ export default function App() {
         .then(response => response.data)
         .then(setMessage)
 
-    const [participants, setParticipants] = useState<Participant[]>([]);
-    useEffect(() => {
-        getAllParticipants()
-    }, [])
-
-    const getAllParticipants = () => {
-        axios.get("/participants")
-            .then(response => response.data)
-            .then(setParticipants)
-    }
-
     return (<>
             <h1>{message}</h1>
-            <h2>{participants.map(participant =>
-                participant.name)}</h2>
         </>
     );
 }
