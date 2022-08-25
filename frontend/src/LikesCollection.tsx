636e88a9fc00d37a96992f81547fa3811f372bbc
sonia <sofiashchukina@outlook.com>
2022-08-25 11:30:12 +0200

    change frontend to avoid empty submissions

--- frontend/src/LikesCollection.tsx
+++ frontend/src/LikesCollection.tsx
@@ -12,26 +12,23 @@ export default function LikesCollection(props:
 
 
     const [likerName, setLikerName] = useState<string>("");
-
+    const [likedNames, setLikedNames] = useState<string[]>([]);
+    const [resultMessage, setResultMessage] = useState<string>("");
     const handleLikerChange = (event: ChangeEvent<HTMLSelectElement>) => {
         setLikerName(event.target.value);
     }
-    const [likedNames, setLikedNames] = useState<string[]>([]);
-
 
     const handleLikedChange = (event: ChangeEvent<HTMLInputElement>) => {
         if (event.currentTarget.checked) {
             setLikedNames([...likedNames, event.target.value])
         } else {
-            const updatedLikedNames: string[] = [];
-            likedNames.forEach((name: string) => {
-                if (name !== event.target.value) {
-                    updatedLikedNames.push(name);
-                }
-            })
+            const updatedLikedNames = likedNames.filter((name) => {
+                return name !== event.target.value
+            });
             setLikedNames(updatedLikedNames);
         }
     }
+
     useEffect(() => {
         if (props.participants && props.participants.length > 0) {
             setLikerName(props.participants[0].name);
@@ -50,10 +47,12 @@ export default function LikesCollection(props:
                 likedParticipants.push(likedParticipant)
             }
         })
-        if (likerParticipant && likedParticipants) {
+        if (likerParticipant && likedParticipants.length > 0) {
             props.sendLike(likerParticipant, likedParticipants);
-            setLikerName("");
-            setLikedNames([]);
+            setLikedNames([])
+            setResultMessage("Your likes were succesfully submitted, you'll receive contacts of your matches soon");
+        } else {
+            setResultMessage("Your likes were not submitted, please check, that you liked minimum one person")
         }
     }
 
@@ -69,14 +68,16 @@ export default function LikesCollection(props:
             <div id="checkboxes">
                 {props.participants.map((participant) =>
                     (<div id="onePersonCheck">
-                        <input key={participant.name} type="checkbox"
+                        <input type="checkbox" className="checkbox" key={participant.name}
                                onChange={handleLikedChange}
-                               value={participant.name}></input>
+                               value={participant.name}
+                        ></input>
                         <label> {participant.name}</label>
                     </div>))}
             </div>
 
             <Button type="submit" id="saveButton" variant="contained" endIcon={<SendIcon/>}>save</Button>
+            <div id="resultMessage">{resultMessage} </div>
         </form>
 
     );
