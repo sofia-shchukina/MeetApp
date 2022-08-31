8ce564ca7feac71c02c26ceb6782f70ec7429bb6
sonia <sofiashchukina@outlook.com>
2022-08-31 09:29:28 +0200

    avoid vulnerability

--- backend/src/main/java/sonia/meetapp/app/participants/SecurityConfig.java
+++ backend/src/main/java/sonia/meetapp/app/participants/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(appUserDetailsService);
+        auth.userDetailsService(appUserDetailsService).passwordEncoder(new BCryptPasswordEncoder());
     }
 
     @Bean
