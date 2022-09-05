351a7dc6dde3e7dc53311cc6dbfb0da99b92c7cd
sonia <sofiashchukina@outlook.com>
2022-09-05 19:32:57 +0200

    delete not used exception

--- backend/src/main/java/sonia/meetapp/exceptions/GlobalExceptionHandler.java
+++ backend/src/main/java/sonia/meetapp/exceptions/GlobalExceptionHandler.java
@@ -56,10 +56,4 @@ public class GlobalExceptionHandler {
         Map<String, Object> responseBody = hashmapBuilder(exception.getMessage());
         return new ResponseEntity<>(responseBody, HttpStatus.BAD_REQUEST);
     }
-
-    @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<Map<String, Object>> handleUserNotFoundException(UserNotFoundException exception) {
-        Map<String, Object> responseBody = hashmapBuilder(exception.getMessage());
-        return new ResponseEntity<>(responseBody, HttpStatus.NOT_FOUND);
-    }
 }
